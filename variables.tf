ab4229d5d051bec71afe022e56cd469a32e38fdc
ibabyyy <110555899+ibabyyy@users.noreply.github.com>
2023-09-08 12:03:44 +0200

    asfdsadf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "bucketname" {
+    default = "marcorobinproject"
+}
+
+variable "access_key" {
+    default = "ASIAS77B3BTLTF6P3NKO"
+}
+
+variable "secret_key" {
+    default = "3l+z1rmeaiCNG6tb3K9r56cg6eUZCAR7s2TQ2Zgg"
+}
