ec69fe7078d8a4d1ac880130d139b27688aac1f0
ibabyyy <110555899+ibabyyy@users.noreply.github.com>
2023-09-12 13:50:20 +0200

    dsada

--- main.tf
+++ main.tf
@@ -3,3 +3,75 @@ resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
 }
 
+#rechte vergeben
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id 
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+#bucket  public machen
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+#bucket öffenbtlich amchen 
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+
+#s3 index objekt hochladen
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "idnex.html"
+  source = "index.html"
+  #objekt public machen
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+#s3 error objekt hochladen
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "error.html"
+  source = "error.html"
+  #objekt public machen
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+#s3 profile objekt hochladen
+resource "aws_s3_object" "profile" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "profile.png"
+  source = "profile.png"
+  acl = "public-read"
+}
+
+#website erstellen
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [aws_s3_bucket_acl.example]
+}
